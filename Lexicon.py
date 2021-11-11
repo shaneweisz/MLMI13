4a743e09917ec956eb3d307819d9fa125a0c676f
Shane Weisz <shaneweisz@gmail.com>
2021-11-11 11:00:07 +0000

    Revert "Convert tokens to lower case when reading in data"
    This reverts commit 47a00523647bf000de4825696188087851751dbe.
    
    The reason for this revert is that this is a part of stemming. So in
    order to do a proper stemming vs non-stemming experiment, we keep the
    case difference around initially.

--- Lexicon.py
+++ Lexicon.py
@@ -44,10 +44,10 @@ class SentimentLexicon(Evaluation):
             tokens = review[1]
 
             for token, pos_tag in tokens:
-                if token not in self.lexicon:
+                if token.lower() not in self.lexicon:
                     continue # skip punctuation and words not in the lexicon
 
-                token_magnitude, token_polarity = self.lexicon[token]
+                token_magnitude, token_polarity = self.lexicon[token.lower()]
 
                 if token_polarity == 'positive':
                     if magnitude:
